6c52e84fd753a5ab65a666d16dc2cea187b7e4f8
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-03-24 21:26:51 +0700

    produk user

--- application/views/user/Home/index.php
+++ application/views/user/Home/index.php
@@ -1051,36 +1051,35 @@
       <div class="container" data-aos="fade-up">
 
         <header class="section-header">
-          <h2>Blog</h2>
-          <p>Recent posts form our Blog</p>
+          <p>Produk Kami</p>
         </header>
 
         <div class="row">
 
           <div class="col-lg-4">
             <div class="post-box">
-              <div class="post-img"><img src="assets/img/blog/blog-1.jpg" class="img-fluid" alt=""></div>
-              <span class="post-date">Tue, September 15</span>
-              <h3 class="post-title">Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit</h3>
-              <a href="blog-singe.html" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+              <div class="post-img"><img src="assets/img/produk/1.jpg" class="img-fluid" alt=""></div>
+              <span class="post-date">Rp. 20.000.000</span>
+              <h3 class="post-title">Paket Cafe Micro (DELONGHI-2302)</h3>
+              <a href="blog-singe.html" class="readmore stretched-link mt-auto"><span>Beli Produk</span><i class="bi bi-arrow-right"></i></a>
             </div>
           </div>
 
           <div class="col-lg-4">
             <div class="post-box">
-              <div class="post-img"><img src="assets/img/blog/blog-2.jpg" class="img-fluid" alt=""></div>
-              <span class="post-date">Fri, August 28</span>
-              <h3 class="post-title">Et repellendus molestiae qui est sed omnis voluptates magnam</h3>
-              <a href="blog-singe.html" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+              <div class="post-img"><img src="assets/img/produk/2.jpg" class="img-fluid" alt=""></div>
+              <span class="post-date">Rp. 15.000.000</span>
+              <h3 class="post-title">Paket Cafe Komersial Kecil (LAPAVONI-2302)</h3>
+              <a href="blog-singe.html" class="readmore stretched-link mt-auto"><span>Beli Produk</span><i class="bi bi-arrow-right"></i></a>
             </div>
           </div>
 
           <div class="col-lg-4">
             <div class="post-box">
-              <div class="post-img"><img src="assets/img/blog/blog-3.jpg" class="img-fluid" alt=""></div>
-              <span class="post-date">Mon, July 11</span>
-              <h3 class="post-title">Quia assumenda est et veritatis aut quae</h3>
-              <a href="blog-singe.html" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+              <div class="post-img"><img src="assets/img/produk/3.jpg" class="img-fluid" alt=""></div>
+              <span class="post-date">Rp. 17.000.000</span>
+              <h3 class="post-title">Paket Cafe Micro (WELHOME-2302)</h3>
+              <a href="blog-singe.html" class="readmore stretched-link mt-auto"><span>Beli Produk</span><i class="bi bi-arrow-right"></i></a>
             </div>
           </div>
 
