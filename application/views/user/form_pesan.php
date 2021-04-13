8e5088e0e38772f7c1032b66fddcf312d183f00c
agungwahyu23 <52806335+agungwahyu23@users.noreply.github.com>
2021-04-13 16:53:03 +0700

    form pesan

--- application/views/user/form_pesan.php
+++ application/views/user/form_pesan.php
@@ -21,15 +21,14 @@
     				<div class="col-lg-12">
     					<div class="card shadow-lg border-0 rounded-lg mt-15">
     						<div class="card-header justify-content-center">
-    							Data Pemesanan
+    							<h3> Data Pemesanan </h3>
     						</div>
     						<div class="card-body">
     							<div class="col">
 
-    								<form action="<?= base_url('Checkout/Post') ?>" method="post"
+    								<form action="<?= base_url('Pesan/Post') ?>" method="post"
     									enctype="multipart/form-data">
 
-    									<h3 class="sub-title">Data Penerima</h3>
     									<div class="row">
     										<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"><label class="small mb-1"
     												for="nama_lengkap">Nama Lengkap</label>
@@ -42,26 +41,22 @@
     										</div>
     									</div>
     									<div class="row">
-    										<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12"><label
+    										<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"><label
     												class="small mb-1" for="nomor_telepon">Nomor Telepon</label>
     											<input type="text" class="form-control mb-3" id="nomor_telepon"
-    												name="nomor_telepon" placeholder="Nomor Telepon" required>
+    												name="nomor_telepon" onkeypress="return event.charCode >= 48 && event.charCode <=57" placeholder="Nomor Telepon" required>
     										</div>
-    										<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12"><label
-    												class="small mb-1" for="alamat">Alamat</label>
-    											<input type="text" class="form-control mb-3" id="alamat" name="alamat"
-    												placeholder="Alamat" required>
+    										<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12"><label
+    												class="small mb-1" for="nomor_telepon">Alamat lengkap</label>
+    											<input type="text" class="form-control mb-3" id="nomor_telepon"
+    												name="alamat" placeholder="Alamat lengkap" required>
     										</div>
-    										<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12"><label
-    												class="small mb-1" for="alamat">Alamat</label>
-    											<input type="text" class="form-control mb-3" id="alamat" name="alamat"
-    												placeholder="Alamat" required>
-    										</div>
+    										
     									</div>
     									
 
     									<div class="d-flex justify-content-end">
-    										<button type="submit" class="btn btn-primary" type="button" id="start">
+    										<button type="submit" class="btn btn-success" type="button" id="start">
     											Checkout
     										</button>
     										
