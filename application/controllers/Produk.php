3c5fdfef200201f5dfe8f200445c6388cdf3e754
agungwahyu23 <52806335+agungwahyu23@users.noreply.github.com>
2021-04-14 13:05:52 +0700

    responsiv produk

--- application/controllers/Produk.php
+++ application/controllers/Produk.php
@@ -6,7 +6,62 @@ class Produk extends CI_Controller {
     public function index()
     {
         $data['produk'] = $this->db->get('produk')->result_array();
-        $this->load->view('user/produk', $data);   
+
+        if ($this->input->post('cari')) {
+            $cari = $this->input->post('cari');
+            $this->db->like('namaProduk', $cari);
+            $total = $this->db->join('satuanProduk', 'satuanProduk.idSatuan = produk.idSatuan')->get_where('produk', ['status' => 1])->num_rows();
+        } else {
+            //ambil data total
+            $total = $this->db->count_all('produk');
+            $cari = null;
+        }
+        
+        $this->load->library('pagination'); // Load librari paginationnya
+
+
+        //konfigurasi pagination
+        $config['base_url']                = base_url() . 'produk/index/';
+        $config['total_rows']            =  $total;
+        $config['use_page_numbers']        = TRUE;
+        $config['per_page']                = 12;
+        $config['uri_segment']            = 3;
+        $config['num_links']            = 5;
+
+        $config['first_link']       = 'First';
+        $config['last_link']        = 'Last';
+        $config['next_link']        = 'Next';
+        $config['prev_link']        = 'Prev';
+        $config['full_tag_open']    = '<div class="pagging text-center"><nav><ul class="pagination justify-content-center">';
+        $config['full_tag_close']   = '</ul></nav></div>';
+        $config['num_tag_open']     = '<li class="page-item"><span class="page-link">';
+        $config['num_tag_close']    = '</span></li>';
+        $config['cur_tag_open']     = '<li class="page-item active"><span class="page-link">';
+        $config['cur_tag_close']    = '<span class="sr-only"></span></span></li>';
+        $config['next_tag_open']    = '<li class="page-item"><span class="page-link">';
+        $config['next_tagl_close']  = '<span aria-hidden="true">&raquo;</span></span></li>';
+        $config['prev_tag_open']    = '<li class="page-item"><span class="page-link">';
+        $config['prev_tagl_close']  = '</span>Next</li>';
+        $config['first_tag_open']   = '<li class="page-item"><span class="page-link">';
+        $config['first_tagl_close'] = '</span></li>';
+        $config['last_tag_open']    = '<li class="page-item"><span class="page-link">';
+        $config['last_tagl_close']  = '</span></li>';
+
+        $config['first_url']            = base_url() . 'produk/index';
+
+        $this->pagination->initialize($config); // Set konfigurasi paginationnya
+        $page         = ($this->uri->segment(3)) ? ($this->uri->segment(3) - 1) * $config['per_page'] : 0;
+
+        if ($cari) {
+            $this->db->like('nama', $cari);
+            $data['produk'] = $this->db->get_where('produk', ['status' => 1] ,$config['per_page'], $page)->result_array();
+        } else {
+            $data['produk'] = $this->db->get_where('produk', ['status' => 1], $config['per_page'], $page)->result_array();
+        }
+
+        $data['pagination'] = $this->pagination->create_links();
+
+        $this->load->view('user/produk', $data);  
     }
 
     public function detail($id = null)
--- application/views/user/detail_produk.php
+++ application/views/user/detail_produk.php
@@ -36,7 +36,7 @@
 					<div class="portfolio-info">
 						<h3><?= $produk['nama']?> <?= $produk['kapasitas']?> Gram</h3>
 						<h6>Harga:</h6>
-                        <h3>Rp. <?= number_format($produk['harga'], 2)?></h3>
+                        <h3>Rp. <?= number_format($produk['harga'], 0, ",", ".")?></h3>
                         <!-- <a href="<?= base_url('Pesan') ?>" class="btn btn-pesan">Pesan Sekarang</a> -->
                         <a href="https://api.whatsapp.com/send?phone=<?= $profile['wa'] ?>&text=Selamat Pagi%21%20Saya%20berminat%20membeli%20<?= $produk['nama']?>" class="btn btn-pesan">Pesan Sekarang</a>
 					</div>
