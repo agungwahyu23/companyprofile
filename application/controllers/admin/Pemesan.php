d7bc4fc76e6394ad9fe22dbd0b463d0e2fba1b3d
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-04-16 12:48:40 +0700

    .

--- application/controllers/admin/Pemesan.php
+++ application/controllers/admin/Pemesan.php
@@ -1,30 +1,30 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Kritik extends CI_Controller
+class Pemesan extends CI_Controller
 {
     public function __construct()
 	{
 		parent::__construct();
 		belumlogin();
 	}
-    
+
     public function index()
     {
-        $data['kritik'] = $this->db->get('kritik')->result_array();
-        $this->load->view('admin/kritik/kritik', $data);
+        $data['pemesan'] = $this->db->get('transaksi')->result_array();
+        $this->load->view('admin/pemesan/pemesan', $data);
     }
 
     public function hapus($id = null)
     {
         if ($id) {
-            $this->db->delete('kritik', ['idKritik' => $id]);
+            $this->db->delete('transaksi', ['idTransaksi' => $id]);
             $this->session->set_flashdata('pesan', '<div class="alert alert-success" role="alert">
-						Berhasil Menghapus Kritik Pengunjung
+						Berhasil Menghapus Pesanan Pengunjung
 						</div>');
-            redirect('admin/Kritik');
+            redirect('admin/Pemesan');
         } else {
-            redirect('admin/Kritik');
+            redirect('admin/Pemesan');
         }
     }
 }
